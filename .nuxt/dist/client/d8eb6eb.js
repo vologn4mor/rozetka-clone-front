427336ce57ec734d3c41593b1eb123ee54515956
volognamor <romangolov2001@gmail.com>
2023-03-28 00:40:26 +0300

    add gh-pages

---
+++ .nuxt/dist/client/d8eb6eb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{372:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},c=t(61),component=Object(c.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ .nuxt/nuxt-i18n/options.js
@@ -0,0 +1,41 @@
+export const Constants = {
+  COMPONENT_OPTIONS_KEY: "nuxtI18n",
+  STRATEGIES: {"PREFIX":"prefix","PREFIX_EXCEPT_DEFAULT":"prefix_except_default","PREFIX_AND_DEFAULT":"prefix_and_default","NO_PREFIX":"no_prefix"},
+  REDIRECT_ON_OPTIONS: {"ALL":"all","ROOT":"root","NO_PREFIX":"no prefix"},
+}
+export const nuxtOptions = {
+  isUniversalMode: true,
+  trailingSlash: undefined,
+}
+export const options = {
+  vueI18n: {"fallbackLocale":"ua","messages":{"ru":{"categories":"Категории","iAmLooking":"Я ищу...","found":"Найти"},"ua":{"categories":"Категорії","iAmLooking":"Я шукаю...","found":"Знайти"}}},
+  vueI18nLoader: false,
+  locales: ["ua","ru"],
+  defaultLocale: "ua",
+  defaultDirection: "ltr",
+  routesNameSeparator: "___",
+  defaultLocaleRouteNameSuffix: "default",
+  sortRoutes: true,
+  strategy: "prefix_except_default",
+  lazy: false,
+  langDir: null,
+  rootRedirect: null,
+  detectBrowserLanguage: {"alwaysRedirect":false,"cookieAge":365,"cookieCrossOrigin":false,"cookieDomain":null,"cookieKey":"i18n_redirected","cookieSecure":false,"fallbackLocale":"","redirectOn":"root","useCookie":true},
+  differentDomains: false,
+  baseUrl: "",
+  vuex: {"moduleName":"i18n","syncRouteParams":true},
+  parsePages: true,
+  pages: {},
+  skipSettingLocaleOnNavigate: false,
+  onBeforeLanguageSwitch: () => {},
+  onLanguageSwitched: () => null,
+  normalizedLocales: [{"code":"ua"},{"code":"ru"}],
+  localeCodes: ["ua","ru"],
+  additionalMessages: [],
+}
+export const localeFiles = {
+  0: "{",
+  1: "}",
+}
+
+export const localeMessages = {}
