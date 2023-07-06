544cb91da5ed30e95d84b146a040955fd5963821
oleg <oleg@timtyler.org.ua>
2023-07-06 19:58:20 +0300

    - in process gift certs

---
+++ components/ui/GreenCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="head">
+      <slot name="head"/>
+    </div>
+    <div class="content">
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GreenCard"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/other/gift-certificates.vue
+++ pages/other/gift-certificates.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="certificates">
-    <h1>Certificates</h1>
+    <h1>{{ $t('giftCertificates') }}</h1>
+    <app-card class="head-card" :width="948">
+
+    </app-card>
+
   </div>
 </template>
 
 <script>
+import {mapMutations} from "vuex";
+import AppCard from "@/components/ui/AppCard";
+
 export default {
-  name: "GiftCertificates"
+  name: "GiftCertificates",
+  components: {AppCard},
+  beforeMount() {
+    const header = this.$i18n.t('giftCertificates')
+    this.setHeaderLocate([{id:0, name:header}]);
+  },
+  methods: {
+    ...mapMutations({
+      setHeaderLocate: 'setHeaderLocate',
+    }),
+  },
+
 }
 </script>
 
 <style scoped>
 
+.certificates{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 35px;
+  text-align: center;
+  font-weight: 700;
+  font-family: Mariupol-Bold, serif;
+}
+
+.head-card {
+
+}
 </style>
