1352f777bba61c8ecbc71eabbd029c1d1ccc0f68
oleg <oleg@timtyler.org.ua>
2023-07-06 11:38:55 +0300

    - initial gift certs

---
+++ pages/other/gift-certificates.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="certificates">
+    <h1>Certificates</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiftCertificates"
+}
+</script>
+
+<style scoped>
+
+</style>
