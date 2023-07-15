3fc4c25de5b321f2035ede317d65ada685a69dc6
oleg <oleg@timtyler.org.ua>
2023-07-15 16:17:02 +0300

    - premium in process

---
+++ components/ui/AppImgCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class='app-card' :style="'width: ' + width + 'px;'" @click='$emit("click")'>
+    <div class="img-start">
+      <slot name="img-start"></slot>
+    </div>
+    <div class='app-card-content'>
+      <slot name="content" />
+    </div>
+    <div class="img-end">
+      <slot name="img-end"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppImgCard',
+  props: {
+    width: {
+      type: String,
+      default: '226',
+    },
+
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+.app-card {
+  background-color: $lh-gray;
+  border-radius: 12px;
+
+  display: flex;
+  flex-direction: row;
+
+  .app-card-content {
+    padding: 13px;
+    width: 100%;
+    display: flex;
+  }
+}
+
+.img-start {
+  width: max-content;
+}
+
+img {
+  display: block;
+}
+</style>
--- pages/other/premium.vue
+++ pages/other/premium.vue
@@ -1,25 +1,60 @@
 <template>
   <div class="premium">
     <h1>{{ $t('logoPremium') }}</h1>
-    <app-card width="'initial'">
-      <green-card :content-align="'left'">
 
-      </green-card>
-      <img src="~assets/images/Other/Premium/premium_main.png" alt="premium">
-    </app-card>
+    <br>
+    <app-img-card :width="'initial'" >
+      <template #img-end>
+        <img src="~assets/images/Other/Premium/premium.png" alt="premium">
+      </template>
+      <template #content >
+        <div class="ofert">
+          <h3>Переваги підписки Premium</h3>
+
+          <div class="advantages">
+            <div class="icon">
+              <img src="~assets/images/Other/Premium/cam_vector.png"/>
+            </div>
+            <span>Безкоштовна кур’єрська доставка</span>
+          </div>
+
+          <div class="advantages">
+            <div class="icon">
+              <img src="~assets/images/Other/Premium/film_vector.png"/>
+            </div>
+            <span>Фільми, серіали та телевізійні канали</span>
+          </div>
+
+          <div class="advantages">
+            <div class="icon">
+              <img src="~assets/images/Other/Premium/cargo_vector.png"/>
+            </div>
+            <span>Повернення товару кур’єром до 30 днів</span>
+          </div>
+
+          <div class="btn">
+            <app-button :bg-color="'#F45D48'" :text="'Спробувати 1 місяць за 1₴'" @click="$router.push('/')"></app-button>
+          </div>
+
+          <p class="add">А потім: 149₴ на три місяці або 399₴ на 12 місяців</p>
+
+
+        </div>
+      </template>
+    </app-img-card>
 
   </div>
 </template>
 
 <script>
 import {mapMutations} from "vuex";
-import AppCard from "../../components/ui/AppCard";
-import GreenCard from "../../components/ui/GreenCard";
+import AppImgCard from "@/components/ui/AppImgCard";
+import AppButton from "@/components/ui/AppButton";
 
 
 export default {
   name: "Premium",
-  components: {GreenCard, AppCard},
+  components: {AppButton, AppImgCard},
   beforeMount() {
     const header = this.$i18n.t('logoPremium')
     this.setHeaderLocate([{id:0, name:header}]);
@@ -33,6 +68,34 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+.ofert {
+  margin: auto;
+  width: fit-content;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+.advantages {
+  display: flex;
+  margin: 20px 0;
+
+  .icon {
+    margin-right: 6px;
+  }
+}
+
+.btn {
+  margin-top: 25px;
+}
+
+.add {
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 31px;
+}
 </style>
